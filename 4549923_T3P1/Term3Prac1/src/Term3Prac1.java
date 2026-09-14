e3c862003b87ef7a593e169ae31750b7f172bc98
Mfundo Mokhoantle <mokhoantlemfundo@gmail.com>
2026-09-14 22:54:02 +0200

    Class created

---
+++ 4549923_T3P1/Term3Prac1/src/Term3Prac1.java
@@ -0,0 +1,17 @@
+// Surname: Mokhoantle
+// Name: Mfundo
+// Student no: 4549923
+// Course: CSC212
+// Year: 2026
+// Assignment: Practical 1 Term 3
+// File: Term3Prac1.java
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Term3Prac1 {
+
+}
